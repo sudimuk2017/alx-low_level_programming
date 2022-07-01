de6095f9caae4b9f417fda4f8d1089e594eb0f19
Sudi Murindanyi <murindanyi@gmail.com>
2022-06-30 17:02:55 -0700

    Hello and puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*
-* Should end with the value 0 
-*/
+
+/**
+ * The program end with 0 
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
