694d24d40d6f7d56959c04be5783d04d4fef007f
Sudi Murindanyi <murindanyi@gmail.com>
2022-07-01 17:51:28 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: zero
+ */
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++){
+		putchar(c);
+		putchar('\n');
+	}
+return (0);
+}
