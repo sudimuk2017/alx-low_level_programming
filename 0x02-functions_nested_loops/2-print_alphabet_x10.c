e30c1e9793de0f903131faff0e6f9c30a20f830c
Sudi Murindanyi <murindanyi@gmail.com>
2022-07-06 17:35:26 -0700

    print alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
-  * print_alphabet_x10 - repeats the print_alphabet 10 times.
-  * Return: Nothing.
-  */
+ * main - print_alphabet_x10
+ *
+ * Return: zero
+ */
 void print_alphabet_x10(void)
 {
 	int i;
