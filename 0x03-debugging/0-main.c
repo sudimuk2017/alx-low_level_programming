69349bd9f0d8ab8a7994cca42f04e57f77ba8344
Sudi Murindanyi <murindanyi@gmail.com>
2022-07-07 08:59:47 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test the number printed is positive or negative
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
